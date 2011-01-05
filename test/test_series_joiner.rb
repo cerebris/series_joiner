2692d3e84286f52cba31a51d386a786352cd170f
Dan Gebhardt <dan@cerebris.com>
2011-01-05 11:55:10 -0500

    Initial commit to series_joiner.

---
+++ test/test_series_joiner.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestSeriesJoiner < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
